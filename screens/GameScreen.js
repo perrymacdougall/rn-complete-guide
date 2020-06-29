5e6f7e6dc15035e13d178a565da4db12c5bfcd6f
perrymacdougall <perrymacdougall@gmail.com>
2020-06-29 02:35:02 -0400

    created GameOverScreen and wrote logic to determine when it should be displayed

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { View, Text, StyleSheet, Button, Alert } from 'react-native';
 
 import NumberContainer from '../components/NumberContainer';
@@ -19,10 +19,18 @@ const GameScreen = props => {
     const [currentGuess, setCurrentGuess] = useState(
         generateRandomBetween(1, 100, props.userChoice)
     );
-    
+    const [rounds, setRounds] = useState(0);
     // Values stored as refs persist on component re-render
     const currentLow = useRef(1);
     const currentHigh = useRef(100);
+
+    const { userChoice, onGameOver } = props;
+
+    useEffect(() => {
+        if (currentGuess === userChoice) {
+            onGameOver(rounds);
+        }
+    }, [currentGuess, userChoice, onGameOver]);
 
     const nextGuessHandler = direction => {
         if (
@@ -39,8 +47,13 @@ const GameScreen = props => {
         } else {
             currentLow.current = currentGuess;
         }
-        const nextNumber = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess);
+        const nextNumber = generateRandomBetween(
+            currentLow.current, 
+            currentHigh.current, 
+            currentGuess
+        );
         setCurrentGuess(nextNumber);
+        setRounds(curRounds => curRounds + 1);
     };
 
     return (
