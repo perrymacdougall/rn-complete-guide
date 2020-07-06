1859a3fd3b9c2819c0c5ea876b841a8d72ae6f8a
perrymacdougall <perrymacdougall@gmail.com>
2020-07-06 02:48:45 -0400

    used MainButton component for Lower and Greater buttons

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -3,6 +3,7 @@ import { View, Text, StyleSheet, Button, Alert } from 'react-native';
 
 import NumberContainer from '../components/NumberContainer';
 import Card from '../components/Card';
+import MainButton from '../components/MainButton';
 
 const generateRandomBetween = (min, max, exclude) => {
     min = Math.ceil(min);
@@ -61,8 +62,8 @@ const GameScreen = props => {
             <Text>Opponent's Guess</Text>
             <NumberContainer>{currentGuess}</NumberContainer>
             <Card style={styles.buttonContainer}>
-                <Button title="LOWER" onPress={nextGuessHandler.bind(this, 'lower')} />
-                <Button title="GREATER" onPress={nextGuessHandler.bind(this, 'greater')} />
+                <MainButton onPress={nextGuessHandler.bind(this, 'lower')}>Lower</MainButton>
+                <MainButton onPress={nextGuessHandler.bind(this, 'greater')}>Greater</MainButton>
             </Card>
         </View>
     )
